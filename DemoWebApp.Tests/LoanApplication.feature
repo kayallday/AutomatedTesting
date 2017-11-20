90b64baf87b18fd5576a6aad939800bd2ca95f50
KayBeard <kbeard@apterainc.com>
2017-11-20 10:56:38 -0500

    feat: generate step definitions begin using spec flow

--- DemoWebApp.Tests/LoanApplication.feature
+++ DemoWebApp.Tests/LoanApplication.feature
@@ -10,4 +10,12 @@ Scenario: Application completed successfully
 		And I select that I have an existing loan account 
 		And I confirm my acceptance of the terms and conditions 
 	When I submit my application
-	Then I should the application complete confirmation for Sarah
+	Then I should see the application complete confirmation for Sarah
+
+Scenario: Cannot submit application unless terms and conditions accepted
+	Given: I am on the loan application screen
+		And I enter a first name of Gentry 
+		And I enter a second name of Jones
+		And I select that I have an existing loan account
+	When I submit my application
+	Then I should see an error message telling me I must accept the terms and conditions
--- DemoWebApp.Tests/LoanApplication.feature.cs
+++ DemoWebApp.Tests/LoanApplication.feature.cs
@@ -93,7 +93,29 @@ this.ScenarioSetup(scenarioInfo);
 #line 12
  testRunner.When("I submit my application", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line 13
- testRunner.Then("I should the application complete confirmation for Sarah", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+ testRunner.Then("I should see the application complete confirmation for Sarah", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+            this.ScenarioCleanup();
+        }
+        
+        [Xunit.FactAttribute(DisplayName="Cannot submit application unless terms and conditions accepted")]
+        [Xunit.TraitAttribute("FeatureTitle", "LoanApplication")]
+        [Xunit.TraitAttribute("Description", "Cannot submit application unless terms and conditions accepted")]
+        public virtual void CannotSubmitApplicationUnlessTermsAndConditionsAccepted()
+        {
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Cannot submit application unless terms and conditions accepted", ((string[])(null)));
+#line 15
+this.ScenarioSetup(scenarioInfo);
+#line 17
+  testRunner.And("I enter a first name of Gentry", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 18
+  testRunner.And("I enter a second name of Jones", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 19
+  testRunner.And("I select that I have an existing loan account", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 20
+ testRunner.When("I submit my application", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 21
+ testRunner.Then("I should see an error message telling me I must accept the terms and conditions", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
             this.ScenarioCleanup();
         }
